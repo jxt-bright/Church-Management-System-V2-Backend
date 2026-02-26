cd50a7306fec9acc53387b1d60b2b87a9d3e8368
Jxt_bright <asiedudansobrempong@gmail.com>
2026-02-26 11:54:13 +0000

    Debugging CORS

--- src/app.js
+++ src/app.js
@@ -27,13 +27,15 @@ const app = express();
 
 
 // Middlewares
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(helmet());
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN,
-//   credentials: true
-// }))
 app.use(cookieParser());
 app.use(passport.initialize());
 passportConfig(passport);
