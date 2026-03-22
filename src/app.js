c2d5dbcdd5cd46515e3f92f8d7938cefbb540c6b
Jxt_bright <asiedudansobrempong@gmail.com>
2026-03-22 00:13:43 +0000

    Updated CORS

--- src/app.js
+++ src/app.js
@@ -28,29 +28,25 @@ const app = express();
 
 // Middlewares
 
-// app.use(cors({ origin: '*' }));
+// Split the environment string into an array
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
 
-// const allowedOrigins = [
-//   process.env.CORS_ORIGIN_1,
-//   process.env.CORS_ORIGIN_2
-// ];
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
-
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN,
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }))
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(helmet());
